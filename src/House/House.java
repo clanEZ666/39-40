33c28b625166002ed149453807f7711df6f02b34
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-07 22:14:53 +0400

    Initial commit

---
+++ src/House/House.java
@@ -0,0 +1,24 @@
+package House;
+
+public class House {
+    private final int floors;
+    private final int rooms;
+    private final int boolean garage;
+
+
+    private House(HouseBuilder builder) {
+        this.floors = builder.floors;
+        this.rooms = builder.rooms;
+        this.garage = builder.garage;
+
+
+
+
+
+
+    }
+
+
+
+
+}
