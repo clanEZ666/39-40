5ac1f483981f8467772d694d52ebf338ab19b26e
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-07 22:40:10 +0400

    Initial commit

--- src/House/House.java
+++ src/House/House.java
@@ -3,7 +3,8 @@ package House;
 public class House {
     private final int floors;
     private final int rooms;
-    private final int boolean garage;
+    private final boolean garage;
+
 
 
     private House(HouseBuilder builder) {
@@ -11,14 +12,50 @@ public class House {
         this.rooms = builder.rooms;
         this.garage = builder.garage;
 
-
-
-
-
-
+    }
+    public String isGarage() {
+        return garage ? "Да" : "Нет";
     }
 
+    public int getRooms() {
+        return rooms;
+    }
+
+    public int getFloors() {
+        return floors;
+    }
+
+    public void infoHouse() {
+        System.out.println("Этажи: " + floors);
+        System.out.println("Комнаты: " + rooms);
+        System.out.println("Есть ли гараж: " + isGarage());
+    }
+    //Родительский класс
+    public static class HouseBuilder {
+        private int floors = 1;
+        private int rooms = 1;
+        private boolean garage = false;
 
 
+    public HouseBuilder setFloors(int floors) {
+        this.floors = floors;
+        return this;
+    }
+
+    public HouseBuilder setRooms(int rooms) {
+        this.rooms = rooms;
+        return this;
+    }
+
+    public HouseBuilder setGarage(boolean garage) {
+        this.garage = garage;
+        return this;
+    }
+
+    public House build() {
+        return new House(this);
+    }
+
+    }
 
 }
--- src/House/Main.java
+++ src/House/Main.java
@@ -1,4 +1,12 @@
 package House;
 
 public class Main {
+    public static void main(String[] args) {
+
+        House house1 = new House.HouseBuilder().setFloors(2).setRooms(5).setGarage(true).build();
+        house1.infoHouse();
+
+        House house2 = new House.HouseBuilder().setFloors(12).setRooms(45).setGarage(false).build();
+        house2.infoHouse();
+    }
 }
