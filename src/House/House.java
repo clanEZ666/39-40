b5eb43b127380ca1ab3e71e46a14d1545ccef8a0
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-21 21:10:56 +0400

    Initial commit

--- src/House/House.java
+++ src/House/House.java
@@ -1,9 +1,11 @@
 package House;
 
+import java.util.Objects;
+
 public class House {
     private final int floors;
     private final int rooms;
-    private final int boolean garage;
+    private final boolean garage;
 
 
     private House(HouseBuilder builder) {
@@ -11,14 +13,70 @@ public class House {
         this.rooms = builder.rooms;
         this.garage = builder.garage;
 
+    }
+
+    public String isGarage() {
+        return garage ? "Да" : "Нет";
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public int getFloors() {
+        return floors;
+    }
+
+    public void infoHouse() {
+        System.out.println("Этажи: " + floors);
+        System.out.println("Комнаты: " + rooms);
+        System.out.println("Есть ли гараж: " + isGarage());
+    }
+
+    //Родительский класс
+    public static class HouseBuilder {
+        private int floors = 1;
+        private int rooms = 1;
+        private boolean garage = false;
 
 
+        public HouseBuilder setFloors(int floors) {
+            this.floors = floors;
+            return this;
+        }
+
+        public HouseBuilder setRooms(int rooms) {
+            this.rooms = rooms;
+            return this;
+        }
+
+        public HouseBuilder setGarage(boolean garage) {
+            this.garage = garage;
+            return this;
+        }
+
+        public House build() {
+            return new House(this);
+        }
+
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            House house = (House) obj;
+            return floors == house.floors &&
+                    rooms == house.rooms&&
+                    garage == house.garage;
+
+
+
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(floors, rooms, garage);
+        }
 
 
 
     }
-
-
-
-
 }
--- src/House/Main.java
+++ src/House/Main.java
@@ -1,4 +1,69 @@
 package House;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        House house1 = new House.HouseBuilder()
+                .setFloors(2).
+                setRooms(5).
+                setGarage(true)
+                .build();
+        house1.infoHouse();
+
+        House house2 = new House.HouseBuilder()
+                .setFloors(12)
+                .setRooms(45)
+                .setGarage(false)
+                .build();
+        house2.infoHouse();
+
+        House house3 = new House.HouseBuilder()
+                .setFloors(5)
+                .setRooms(4)
+                .setGarage(true)
+                .build();
+        house3.infoHouse();
+
+        House house4 = new House.HouseBuilder()
+                .setFloors(7)
+                .setRooms(7)
+                .setGarage(false)
+                .build();
+        house4.infoHouse();
+
+
+        //Дз 45-46
+
+        ArrayList<House> houses = new ArrayList<House>();
+
+        houses.add(house1);
+        houses.add(house2);
+        houses.add(house3);
+        houses.add(house4);
+
+
+
+        System.out.println("\nПроверка contains:");
+        System.out.println("Содержится ли дом 1: " + houses.contains(house4));
+        System.out.println("Содержится ли дом 2: " + houses.contains(house3));
+
+        // Проверка позиции дома в списке
+        System.out.println("\nПроверка indexOf:");
+        System.out.println("Индекс дома 1: " + houses.indexOf(house2));
+        System.out.println("Индекс дома 2: " + houses.indexOf(house3));
+
+        // Проверка с изменением equals (убираем гараж из сравнения)
+        System.out.println("Проверка с изменением equals: " + houses.equals(house2));
+
+
+        // Проверка equals на наличие дома с гаражом и без гаража
+        System.out.println("Содержится ли дом без учета гаража: " + houses.contains(house4));
+
+
+    }
+
 }
